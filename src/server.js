70075bd67224a8063d0631e44f3bc657840637fa
jason yang <syan19@live.cn>
2021-02-26 06:20:45 +1100

    routes models set up done

---
+++ src/server.js
@@ -0,0 +1,54 @@
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8000
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const flash = require('express-flash')
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://senyang:senyang123@cluster0.aapka.mongodb.net/user_admin?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt')
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
+app.use(bodyParser.json())
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.get('/home', (req, res) => {
+    res.render('home', {title: 'Home Page'})
+})
+
+app.get('/login', (req, res) => {
+    res.render('login', {title: 'Login'})
+})
+
+app.post('/login', (req, res) => {
+
+})
+
+app.get('/register', (req, res) => {
+    res.render('register', {title: 'Register'})
+})
+
+app.post('/register', (req, res) => {
+
+})
+
+app.get('/welcome', (req, res) => {
+    res.render('welcome', {title: 'Welcome'})
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
